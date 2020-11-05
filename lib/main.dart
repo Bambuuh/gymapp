07c75709414a0e9d9eb89f5aacdf78f31f5fbe86
Lukas Klingberg <lukas@klingberg.se>
2020-11-05 23:07:58 +0100

    mived firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,73 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
-import 'package:gymapp/app.dart';
-import 'package:gymapp/screens/loading_screen.dart';
+import 'package:gymapp/providers/exercise_provider.dart';
+import 'package:gymapp/providers/routine_provider.dart';
+import 'package:gymapp/providers/user_provider.dart';
+import 'package:gymapp/providers/workout_provider.dart';
+import 'package:gymapp/screens/add_exercise_screen.dart';
+import 'package:gymapp/screens/add_routine_screen.dart';
+import 'package:gymapp/screens/add_workout_screen.dart';
+import 'package:gymapp/screens/home_screen.dart';
+import 'package:gymapp/screens/login_screen.dart';
+import 'package:gymapp/screens/rest_screen.dart';
+import 'package:gymapp/screens/routine_list_screen.dart';
+import 'package:gymapp/screens/routine_screen.dart';
+import 'package:gymapp/screens/select_repetitions_screen.dart';
+import 'package:gymapp/screens/select_weight_screen.dart';
+import 'package:gymapp/screens/workout_exercise_screen.dart';
+import 'package:gymapp/screens/workout_routine_screen.dart';
+import 'package:gymapp/screens/workout_screen.dart';
+import 'package:gymapp/screens/workout_workout_screen.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(GymApp());
 }
 
-class App extends StatelessWidget {
+class GymApp extends StatelessWidget {
+  // final Color base = Color(0xff333a42);
+  // final Color light = Color(0xff4D545C);
+  // final Color dark = Color(0xff1A2129);
+
   @override
   Widget build(BuildContext context) {
-    final Future<FirebaseApp> _initialization = Firebase.initializeApp();
-    return FutureBuilder(
-        future: _initialization,
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            print('ERROR');
-            print(snapshot.error);
-            return LoadingScreen();
-          }
-          if (snapshot.connectionState == ConnectionState.done) {
-            return GymApp();
-          }
-
-          return LoadingScreen();
-        });
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => RoutineProvider()),
+        ChangeNotifierProvider(create: (_) => WorkoutProvider()),
+        ChangeNotifierProvider(create: (_) => ExerciseProvider()),
+        ChangeNotifierProvider(create: (_) => UserProvider()),
+      ],
+      child: NeumorphicApp(
+        title: 'Gym App',
+        theme: NeumorphicThemeData(
+          intensity: 0.8,
+          lightSource: LightSource.topLeft,
+          // baseColor: base,
+          // shadowLightColor: light,
+          // shadowDarkColor: dark,
+          // shadowLightColorEmboss: light,
+          // shadowDarkColorEmboss: dark,
+        ),
+        home: LoginScreen(),
+        routes: {
+          HomeScreen.routeName: (context) => HomeScreen(),
+          AddRoutineScreen.routeName: (context) => AddRoutineScreen(),
+          WorkoutRoutineScreen.routeName: (context) => WorkoutRoutineScreen(),
+          WorkoutWorkoutScreen.routeName: (context) => WorkoutWorkoutScreen(),
+          WorkoutExerciseScreen.routeName: (context) => WorkoutExerciseScreen(),
+          RoutineScreen.routeName: (context) => RoutineScreen(),
+          WorkoutScreen.routeName: (context) => WorkoutScreen(),
+          RoutineListScreen.routeName: (context) => RoutineListScreen(),
+          AddWorkoutScreen.routeName: (context) => AddWorkoutScreen(),
+          AddExerciseScreen.routeName: (context) => AddExerciseScreen(),
+          SelectRepetitionsScreen.routeName: (context) => SelectRepetitionsScreen(),
+          SelectWeightScreen.routeName: (context) => SelectWeightScreen(),
+          RestScreen.routeName: (context) => RestScreen(),
+        },
+      ),
+    );
   }
 }
